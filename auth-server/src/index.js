34baf97344dfcf979abff4413da021800612f23b
AL <63744576+ALsJourney@users.noreply.github.com>
2025-05-26 15:36:51 +0200

    Add initial auth server with Docker configuration

--- auth-server/src/index.js
+++ auth-server/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { dirname } from 'path';
 
 // Load environment variables
 dotenv.config();
